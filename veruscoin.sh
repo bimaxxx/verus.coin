c4f724673a6a1f73545f0980801bb16ae0198bfd
bimaxxx <89139791+bimaxxx@users.noreply.github.com>
2021-08-18 21:50:34 +0800

    Update veruscoin.sh

--- veruscoin.sh
+++ veruscoin.sh
@@ -1,1 +1,15 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tpc://ap.luckpool.net:3956#xnsub -u RSskTrDyh7oCpirNjxMZxSzr4ubAzMwmfR.dancok -p x --cpu 4
+
+
+
+
+
+
+
+
+
 
